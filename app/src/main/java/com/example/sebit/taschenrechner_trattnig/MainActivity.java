00379b40d30d63f9c29e3bf3db2cac5093ff9748
Sebastian Trattnig <setr780@gmail.com>
2017-03-12 18:06:49 +0100

    *ende*

--- app/src/main/java/com/example/sebit/taschenrechner_trattnig/MainActivity.java
+++ app/src/main/java/com/example/sebit/taschenrechner_trattnig/MainActivity.java
@@ -13,26 +13,26 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {   // Hier brauchen wir nur die onCreate methode
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void Berechnung(View v){
+    public void Berechnung(View v){                              // Methode erstellen auf die button zugreift
 
-        EditText Objekt01=(EditText)findViewById(R.id.zahl01);   // Obejkt Variable erstellen mit R.Id auf layout zugreifen und zahl01 holen also verweiß
-        EditText Objekt02=(EditText)findViewById(R.id.zahl02);
-        EditText Objekt03=(EditText)findViewById(R.id.zahl03);
+        EditText Objekt01=(EditText)findViewById(R.id.zahl01);   // Obejekt Variable erstellen mit
+        EditText Objekt02=(EditText)findViewById(R.id.zahl02);   // R.Id auf layout zugreifen und zahl01 holen
+        EditText Objekt03=(EditText)findViewById(R.id.zahl03);   // also verweiß
 
-        Integer nummer01=Integer.parseInt(Objekt01.getText().toString());
-        Integer nummer02=Integer.parseInt(Objekt02.getText().toString());
+        Integer nummer01=Integer.parseInt(Objekt01.getText().toString());  // hier mit .getText.toString wird String geholt,
+        Integer nummer02=Integer.parseInt(Objekt02.getText().toString());  // da wir aber Int brauchen müssen wir explizit ansprechen
 
-        try {
-
-            Objekt03.setText(String.valueOf(nummer01 / nummer02));
-
+        try {                                    // damit das app nicht abgebrochen wird bei Fehlern wie zb
+                                                 // divison durch Null muss dieser Fehler abgefangen werden dies passiert im catch block
+            Objekt03.setText(String.valueOf(nummer01 / nummer02));   // mit setText wird objekt angesprochen was reinzuschreiben ,
+                                                                     // da ein string gegeben muss umgewandelt werden
         } catch(Exception e) {
-            Objekt03.setText("Div / 0 nicht möglich");
+            Objekt03.setText("Div / 0 nicht möglich");          // Meldung die bei Exception ausgegeben werden soll
 
 
         }
